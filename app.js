487af7247506e45c8502537ecba46bfce7ba5401
Varun Reddy Rajireddy <itsvarun2798@gmail.com>
2023-12-21 15:19:08 -0600

    Used Async/Await and Try/Catch block for Getting data from API

--- app.js
+++ app.js
@@ -5,28 +5,24 @@ let weatherDesc = document.getElementById("weather-desc");
 let searchBtn = document.getElementById("btn");
 let icon = document.getElementById("icon");
 
-const apiKey = '2d9ae582a06d51e56941b7accde258e7';
+const apiKey = "2d9ae582a06d51e56941b7accde258e7";
 
-searchBtn.onclick = () => {
-  if(searchEl.value === ""){
-    alert("Please enter a location");
-  }else{
-    loc = searchEl.value;
-    url = `https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${apiKey}`
-    fetch(url).then(res => res.json())
-    .then(data => {
-      console.log(data);
-      const{name} = data;
-      const{feels_like} = data.main;
-      const{description} = data.weather[0];
-      tempEl.innerText = Math.floor(((feels_like - 273.15) * 1.8 + 32));
-      locEl.innerText = name.toUpperCase();
-      weatherDesc.innerText = description.toUpperCase();
-    })
-    .catch(() => {
-      alert("Enter a valid city");
-    })
-    searchEl.value = ""
+searchBtn.onclick = async () => {
+  try {
+    if (searchEl.value === "") {
+      alert("Please enter a location");
+    } else {
+      loc = searchEl.value;
+      url = `https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${apiKey}`;
+      let res = await fetch(url);
+      let data = await res.json();
+      locEl.innerText = data.name.toUpperCase();
+      weatherDesc.innerText = data.weather[0].description.toUpperCase();
+      tempEl.innerText = Math.floor((data.main.feels_like - 273.15) * 1.8 + 32);
+      searchEl.value = "";
+    }
+  } catch {
+    alert("Enter a valid city");
   }
 };
 
